782da773c492abdb3a1f75b47d0bca9abd33f77c
VoidLance <alistair.m.sweeting@gmail.com>
2026-01-12 20:05:05 +0000

    Add comment to App.jsx for testing deployment

--- Website.code/src/App.jsx
+++ Website.code/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
+// Personal portfolio website app
 import Home from './pages/Home'
 import Games from './pages/Games'
 import Blog from './pages/Blog'
